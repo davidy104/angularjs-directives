41321c7f8f2ed610867ba2e919e4e1ab9e7720be
davidy <david.yuan124@gmail.com>
2015-01-19 17:13:42 +1300

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name directivesTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the directivesTestApp
+ */
+angular.module('directivesTestApp')
+  .controller('MainCtrl', function () {
+  });
